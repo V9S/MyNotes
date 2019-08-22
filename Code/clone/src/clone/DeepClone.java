5a3ab1ee08c337eea4efe3bf52c182bf53e78d1f
V9S <2993953259@qq.com>
2019-08-22 20:34:43 +0800

    克隆

--- Code/clone/src/clone/DeepClone.java
+++ Code/clone/src/clone/DeepClone.java
@@ -2,44 +2,23 @@ package clone;
 
 import java.util.ArrayList;
 
-public class ShallowClone implements Cloneable {
+public class DeepClone implements Cloneable{
 
 	public int a;
-	public Integer b;
-	private int c;
+	public Integer b ;
+	private int c ;
 	public Object o;
 	public ArrayList<Object> t;
-
+	
 	@Override
 	public Object clone() throws CloneNotSupportedException {// 重写Object中的clone()方法
-
-		return super.clone();// 浅克隆直接返回super.clone()
+		
+		DeepClone deepClone = (DeepClone) super.clone();
+//		deepClone.setO((DeepClone)deepClone.getO().clone());
+		deepClone.setT((ArrayList<Object>) deepClone.getT().clone());
+		return deepClone;
 	}
-
-	public int getA() {
-		return a;
-	}
-
-	public void setA(int a) {
-		this.a = a;
-	}
-
-	public Integer getB() {
-		return b;
-	}
-
-	public void setB(Integer b) {
-		this.b = b;
-	}
-
-	public int getC() {
-		return c;
-	}
-
-	public void setC(int c) {
-		this.c = c;
-	}
-
+	
 	public Object getO() {
 		return o;
 	}
@@ -48,10 +27,27 @@ public class ShallowClone implements Cloneable {
 		this.o = o;
 	}
 
+	public int getA() {
+		return a;
+	}
+	public void setA(int a) {
+		this.a = a;
+	}
+	public Integer getB() {
+		return b;
+	}
+	public void setB(Integer b) {
+		this.b = b;
+	}
+	public int getC() {
+		return c;
+	}
+	public void setC(int c) {
+		this.c = c;
+	}
 	public ArrayList<Object> getT() {
 		return t;
 	}
-
 	public void setT(ArrayList<Object> t) {
 		this.t = t;
 	}
@@ -60,5 +56,5 @@ public class ShallowClone implements Cloneable {
 	public String toString() {
 		return "Utils [a=" + a + ", b=" + b + ", c=" + c + ", o=" + o + ", t=" + t + "]";
 	}
-
+	
 }
---
+++ Code/clone/src/clone/Utils.java
@@ -0,0 +1,30 @@
+package clone;
+
+import java.util.ArrayList;
+
+public class Utils {
+
+	public static void main(String[] args) {
+//		ShallowClone origin = new ShallowClone();
+		DeepClone origin = new DeepClone(); 
+		Object n = new Object();
+		ArrayList<Object> t = new ArrayList<>();
+		origin.setA(1);
+		origin.setB(2);
+		origin.setC(3);
+		origin.setO(n);
+		origin.setT(t);
+
+		ShallowClone cloneElement = null;
+		try {
+			cloneElement = (ShallowClone) origin.clone();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println(origin.toString());
+		System.out.println(origin.toString());
+
+		System.out.println(origin.o.equals(cloneElement.o));
+		System.out.println(origin.t.equals(cloneElement.t));
+	}
+}
