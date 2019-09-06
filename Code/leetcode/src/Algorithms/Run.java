8c0414105d0ac7922b0e7d9f524be4b02c267782
V9S <2993953259@qq.com>
2019-09-06 17:46:08 +0800

    update

--- Code/leetcode/src/Algorithms/Run.java
+++ Code/leetcode/src/Algorithms/Run.java
@@ -2,7 +2,7 @@ package Algorithms;
 
 public class Run {
 	public static void main(String[] args) {
-		Q1 q1 =new Q1();
+		twoSum q1 =new twoSum();
 		int temp [] = {2,7,11,15};
 		System.out.println(q1.twoSum(temp, 9));
 	}
--- Code/leetcode/src/Algorithms/twoSum.java
+++ Code/leetcode/src/Algorithms/twoSum.java
@@ -5,12 +5,12 @@ package Algorithms;
  * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
 *你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
  */
-public class Q1 {
+public class twoSum {
 	   public int[] twoSum(int[] nums, int target) {
 			int [] result = new int[2];
 			for (int i = 0; i < nums.length; i++) {
-				for(int m=i;m<nums.length;m++) {
-					if(nums[i]+nums[m]==target&&i!=m) {
+				for(int m=i+1;m<nums.length;m++) {
+					if(nums[i]+nums[m]==target) {
 						result[0]=i;
 						result[1]=m;
 					}
