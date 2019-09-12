2c77f2928c8d78cc2fe96aa550fb14c2e28c90f4
V9S <2993953259@qq.com>
2019-09-12 14:52:46 +0800

    update

--- Code/leetcode/src/Algorithms/Run.java
+++ Code/leetcode/src/Algorithms/Run.java
@@ -2,7 +2,7 @@ package Algorithms;
 
 public class Run {
 	public static void main(String[] args) {
-		twoSum q1 =new twoSum();
+		TwoSum q1 =new TwoSum();
 		int temp [] = {2,7,11,15};
 		System.out.println(q1.twoSum(temp, 9));
 	}
