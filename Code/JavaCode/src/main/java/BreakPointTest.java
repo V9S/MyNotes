c56a02820a413c00468820f1fcccbcde4bfd9fb2
V9S <2993953259@qq.com>
2020-06-22 17:33:34 +0800

    BreakPoint

---
+++ Code/JavaCode/src/main/java/BreakPointTest.java
@@ -0,0 +1,12 @@
+package main.java;
+
+public class BreakPointTest {
+
+    public static void main(String[] args) {
+        int n = 100;
+        for (int m = 0; m <= n; m++) {
+            System.out.println(m);
+        }
+    }
+
+}
